07538a8cb7d5001a5642bf002982d2b535169b53
Cleverson Lopes Ledur <cleverson.ledur@viavarejo.com.br>
2018-10-23 12:29:18 -0300

    Fixing import.

--- src/main/java/com/ef/domain/repository/LogEntryRepository.java
+++ src/main/java/com/ef/domain/repository/LogEntryRepository.java
@@ -1,8 +1,13 @@
 package com.ef.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ef.domain.model.LogEntry;
 
-public interface LogEntryRepository extends CrudRepository<LogEntry, Integer> {
+@Repository public interface LogEntryRepository extends CrudRepository<LogEntry, Integer> {
+
+    List<LogEntry> findByStatus(String status);
 }
