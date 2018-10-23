1808cd2aa3ad0d5774a45bf47393335083eea910
Cleverson Lopes Ledur <cleverson.ledur@viavarejo.com.br>
2018-10-23 12:28:32 -0300

    Refactoring main class.

--- src/main/java/com/ef/Parser.java
+++ src/main/java/com/ef/Parser.java
@@ -1,45 +1,25 @@
 package com.ef;
 
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.util.StringUtils;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
-import com.ef.domain.model.LogEntry;
-import com.ef.service.LogEntryService;
-import com.ef.utils.LogParser;
-import com.ef.utils.OptionParser;
+import com.ef.service.ParserService;
 
-@EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class) @SpringBootApplication public class Parser
-                implements CommandLineRunner {
+@SpringBootApplication public class Parser {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Parser.class, args);
-    }
+    public static void main(String[] args) throws Exception {
 
-    @Autowired LogEntryService logEntryService;
-
-    @Override
-    public void run(String[] args) throws Exception {
-
-        Map<String, String> parameters = OptionParser.parse(args);
-
-        String accessLogParameter = parameters.get(OptionParser.ACCESS_LOG_PARAMETER);
-        String durationParameter = parameters.get(OptionParser.DURATION_PARAMETER);
-        String startDateParameter = parameters.get(OptionParser.START_DATE_PARAMETER);
-        String thresholdParameter = parameters.get(OptionParser.THRESHOLD_PARAMETER);
-
-        if (StringUtils.isEmpty(accessLogParameter)) {
-            List<LogEntry> log = LogParser.parseLog(accessLogParameter);
-            logEntryService.addLogEntryList(log);
+        System.setProperty("spring.config.name", "application");
+        try (final ConfigurableApplicationContext applicationContext = SpringApplication.run(Parser.class, args)) {
+            applicationContext.getBean(ParserService.class).run(args);
         }
-
-        OptionParser.validate(accessLogParameter, durationParameter, startDateParameter, thresholdParameter);
     }
+
+    @Bean
+    ParserService parserService() {
+        return new ParserService();
+    }
+
 }
---
+++ src/main/java/com/ef/service/ParserService.java
@@ -0,0 +1,35 @@
+package com.ef.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import com.ef.domain.model.LogEntry;
+import com.ef.utils.LogParser;
+import com.ef.utils.OptionParser;
+
+@Service public class ParserService implements CommandLineRunner {
+    @Autowired LogEntryService logEntryService;
+
+    @Override
+    public void run(String[] args) throws Exception {
+        Map<String, String> parameters = OptionParser.parse(args);
+
+        String accessLogParameter = parameters.get(OptionParser.ACCESS_LOG_PARAMETER);
+        String durationParameter = parameters.get(OptionParser.DURATION_PARAMETER);
+        String startDateParameter = parameters.get(OptionParser.START_DATE_PARAMETER);
+        String thresholdParameter = parameters.get(OptionParser.THRESHOLD_PARAMETER);
+
+        OptionParser.validate(accessLogParameter, durationParameter, startDateParameter, thresholdParameter);
+
+        if (!StringUtils.isEmpty(accessLogParameter)) {
+            List<LogEntry> log = LogParser.parseLog(accessLogParameter);
+            logEntryService.addLogEntryList(log);
+        }
+
+    }
+}
