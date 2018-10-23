cf5bcc7abba18cfdbab80e2f62f6df99649842e5
Cleverson Lopes Ledur <cleverson.ledur@viavarejo.com.br>
2018-10-23 12:30:39 -0300

    Adjusting test class.

--- src/test/java/com/ef/ParserTests.java
+++ src/test/java/com/ef/ParserTests.java
@@ -1,14 +1,15 @@
 package com.ef;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static junit.framework.TestCase.assertTrue;
 
-@RunWith(SpringRunner.class) @SpringBootTest public class ParserTests {
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest public class ParserTests {
 
     @Test
     public void contextLoads() {
+        assertTrue(true);
     }
 
 }
