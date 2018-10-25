75001a8790580f1a8aee2cc05a2c35c3fbb5380c
Cleverson Lopes Ledur <cleverson.ledur@viavarejo.com.br>
2018-10-25 12:34:20 -0300

    Adding queries

--- SQL_TEST_QUERIES.sql
+++ SQL_TEST_QUERIES.sql
@@ -1,1 +1,7 @@
 SELECT * FROM parser.log_entry;
+
+SELECT * FROM parser.log_entry WHERE date >= '2016-01-01 00:13:00' AND date <= '2017-02-02 00:13:00';
+
+
+SELECT (v.ip, COUNT(v)) FROM parser.log_entry v WHERE v.date >= '2017-01-01 00:00:12' AND v.date <= '2017-01-02 00:00:12' AND COUNT(v)>=10 GROUP BY v.ip ;
+
